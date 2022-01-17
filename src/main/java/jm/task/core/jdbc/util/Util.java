8e398f259fb5731b12506851b02cd947151e483a
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,37 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+
+//    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "springcourse";
+
+    public Connection getDbConnection() {
+        Connection connection = null;
+        try {
+//            DriverManager.registerDriver(driver);
+//            Class.forName(DB_DRIVER);                убрал драйвер, вроде он не нужен с этой версией?
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("connected");
+
+
+
+        } catch (SQLException e) {
+            System.out.println("error");
+            throw new RuntimeException(e);
+
+        }
+        return connection;
+    }
+
 }
