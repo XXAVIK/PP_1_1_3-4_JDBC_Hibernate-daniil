409f211738cd1ad916c880978eff2dad80ed0a87
XXAVIK <ddg1377@gmail.com>
2022-11-14 21:30:11 +0300

    1. Во всех классах сделай ctrl+alt+L, чтобы поправить кодстайл
    2. Util - убери лишние пробелы и закоментированный код
    
    3. Util - сделай метод статическим, далее из дао обращайся к этому методу в конструкторе дао, чтобы получить Connection
    
    4. Main - убрать обращение к util
    
    5. Main - обращаемся к сервису, который в свою очередь обращается к дао
    
    6. UserDaoJDBCImpl - каша в классе, отрефактори. Также сделай рабочие транзакции и роллебки

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,55 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Util util = new Util();
     User user = new User();
-    Connection connection = util.getDbConnection();
-//test git
-    private static Long idCounter = 0L;
-    private final static String CREATE_DB = "CREATE DATABASE IF NOT EXISTS my_db";
-    private final static String USE_DB = "USE my_db";
-    private final static String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS Users (\n" +
+    Connection connection = null;
+    private List<User> userList = new ArrayList<>();
+    private static Long idCounter = 0L;                                                   //Счетчик для поля id
+    private final static String CREATE_DB = "CREATE DATABASE IF NOT EXISTS my_db";        //SQL запрос на создание database
+    private final static String USE_DB = "USE my_db";                                     //SQL запрос на использование database
+    private final static String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS Users" +       //SQL запрос на создание таблицы
+            "(" +
+
             "  id int,\n" +
             "  name varchar(15),\n" +
             "  lastname varchar(25),\n" +
             "  age int\n" +
 
             ");";
-    private final static String DROP_TB = "DROP TABLE IF EXISTS users";
+    private final static String DROP_TB = "DROP TABLE IF EXISTS users";                    //SQL запрос на удаление таблицы
 
-    private final static String SAVE_USER = "INSERT INTO users (id, name, lastName, age) VALUES(?,?,?,?)";
+    private final static String SAVE_USER = "INSERT INTO users (id, name, lastName, age) VALUES(?,?,?,?)"; //Сохранение
 
-    private final static String REMOVE_USER = "DELETE FROM users WHERE id=?";
+    private final static String REMOVE_USER = "DELETE FROM users WHERE id=?";              //Удаление
 
-    private final static String CLEAN_TABLE = "DELETE FROM users";
+    private final static String CLEAN_TABLE = "DELETE FROM users";                         //Очистка таблицы
 
-    private final static String GET_USERS = "SELECT id, name, lastname, age from users";
-    private final static String GET_USER = "Select name, lastname, age from users where id = ?";
-    private List<User> userList = new ArrayList<>();
+    private final static String GET_USERS = "SELECT id, name, lastname, age from users";   // Получение всех данных о USER
+
 
     public UserDaoJDBCImpl() {
-
+        connection = Util.getDbConnection();     // Получаем соединение с БД
     }
 
     public void createUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            statement.execute(CREATE_DB); //если не создавать датабазу, вылетит ошибка в тестах, т.к таблицу некуда поместить
-            statement.execute(USE_DB);    //пробовал запихнуть все эти три команды в одну,
-            // но у меня никак не запускается код так, либо ошибка синтаксиса пишет,либо работает неправильно
+            statement.execute(USE_DB);
+            statement.execute(CREATE_DB);
+            statement.execute(USE_DB);
             statement.execute(CREATE_TABLE);
+
+            connection.commit();
+            connection.rollback();
         } catch (SQLException e) {
-            System.out.println("Error creating db");
+            System.out.println("Error creating Database");
             throw new RuntimeException(e);
         }
     }
 
     public void dropUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            statement.execute(CREATE_DB); //аналогично комм выше
+            statement.execute(CREATE_DB);
             statement.execute(USE_DB);
             statement.execute(DROP_TB);
+
+            connection.commit();
+            connection.rollback();
         } catch (SQLException e) {
-            System.out.println("Error while dropping");
+            System.out.println("Error while dropping table");
             throw new RuntimeException(e);
         }
 
@@ -64,16 +70,20 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         User user1 = new User();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER)) {
-            preparedStatement.setLong(1,++idCounter);
-            preparedStatement.setString(2,name);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER);
+             Statement statement = connection.createStatement()) {
+            statement.execute(CREATE_DB);
+            statement.execute(USE_DB);
+            preparedStatement.setLong(1, ++idCounter);
+            preparedStatement.setString(2, name);
             preparedStatement.setString(3, lastName);
             preparedStatement.setByte(4, age);
-
             preparedStatement.executeUpdate();
-            System.out.println("User с именем – "+name+" добавлен в базу данных, id="+ idCounter);
-            //решил тут немного от себя написать
-            // , надеюсьь ничего страшного)
+
+            connection.commit();
+            connection.rollback();
+            System.out.println("User с именем – " + name + " добавлен в базу данных, id=" + idCounter);
+
         } catch (SQLException e) {
             System.out.println("Error while saving");
             throw new RuntimeException(e);
@@ -81,27 +91,13 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        User user1 = new User();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_USER);
-             PreparedStatement preparedStatement1 = connection.prepareStatement(GET_USER)) {
-            preparedStatement.setLong(1,id);
-            preparedStatement1.setLong(1,id);
-
-            ResultSet resultSet = preparedStatement1.executeQuery();
-            resultSet.next();
-
-
-
-
-            user.setId(id);
-            user.setName(resultSet.getString("name"));
-            user.setLastName(resultSet.getString("lastname"));
-            user.setAge(resultSet.getByte("age"));
+        try (PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_USER)) {
+            preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
 
-            System.out.println("User с именем – "+user.getName()+" ,фамилией "+user.getLastName()+ " - удален, id="+user.getId());
-            //решил тут немного от себя написать, а то не очень понятно тесты норм или нет гоняются
-            // , надеюсьь ничего страшного)
+            connection.commit();
+            connection.rollback();
+            System.out.println("User удален");
         } catch (SQLException e) {
             System.out.println("Error while removing");
             throw new RuntimeException(e);
@@ -111,8 +107,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(GET_USERS);
-
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 user.setId(resultSet.getLong("id"));
                 user.setName(resultSet.getString("name"));
                 user.setLastName(resultSet.getString("lastname"));
@@ -120,6 +115,8 @@ public class UserDaoJDBCImpl implements UserDao {
                 userList.add(user);
                 System.out.println(user);
             }
+            connection.commit();
+            connection.rollback();
         } catch (SQLException e) {
             System.out.println("Error while getting list");
             throw new RuntimeException(e);
@@ -130,11 +127,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (PreparedStatement preparedStatement = connection.prepareStatement(CLEAN_TABLE)) {
             preparedStatement.executeUpdate();
+
+            connection.commit();
+            connection.rollback();
             System.out.println("Таблица очищена");
         } catch (SQLException e) {
             System.out.println("Error while cleaning");
             throw new RuntimeException(e);
         }
-        idCounter =0L;
+        idCounter = 0L;  //Сброс счетчика id после очистки таблицы
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,33 +3,23 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Util {
 
-
-//    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "springcourse";
 
-    public Connection getDbConnection() {
+    public static Connection getDbConnection() {
         Connection connection = null;
         try {
-//            DriverManager.registerDriver(driver);
-//            Class.forName(DB_DRIVER);                убрал драйвер, вроде он не нужен с этой версией?
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            connection.setAutoCommit(false);
             System.out.println("connected");
-
-
-
         } catch (SQLException e) {
             System.out.println("error");
             throw new RuntimeException(e);
-
         }
         return connection;
     }
